e649da6c0f36593476695bc17436832c4ba6d2d5
aes-alienrip <73251414+aes-alienrip@users.noreply.github.com>
2023-09-20 21:55:47 +0800

    Update version number to 1.1.9

--- dist/hko-weather-card.js
+++ dist/hko-weather-card.js
@@ -5,7 +5,7 @@ import {
 
 // #### Add card info to console
 console.info(
-  `%cHKO-WEATHER-CARD\n%cVersion 1.1.8a       `,
+  `%cHKO-WEATHER-CARD\n%cVersion 1.1.9       `,
   "color: #043ff6; font-weight: bold; background: white",
   "color: white; font-weight: bold; background: #043ff6"
 );
